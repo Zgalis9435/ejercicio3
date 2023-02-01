12c1a0a54bd576a002a81856f15b2b71d0625af1
Zgalis9435 <zharos.delgado@protonmail.com>
2023-02-01 20:38:27 +0100

    Initial commit

---
+++ src/Coche.java
@@ -0,0 +1,19 @@
+public class Coche {
+    private int puertas;
+
+    public Coche = new Coche (int puertas){
+        puertas = this.puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
+
+    public getPuertas() {
+        return puertas;
+    }
+    public int sumaPuertas(int puertas){
+        puertas++;
+        System.out.println("Este coche tiene: "+puertas+" puertas.");
+    }
+}
